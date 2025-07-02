170364d855b7a9ef3703f278937b4f9d97ef1bd5
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-02 10:53:53 +0800

    Update vite config.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vuejs-github-pages/',
   plugins: [
     vue(),
     vueDevTools(),
